7029a65aa3d8ed0042c9e9acc2bb7617bd3c8b20
eug-darnanz <darnanz@campus.eug.es>
2021-01-20 19:55:12 +0000

    Carrusel and option

--- functions/index.js
+++ functions/index.js
@@ -11,6 +11,7 @@ const {
     Suggestions,
     BrowseCarousel,
     BrowseCarouselItem,
+    LinkOutSuggestion,
 } = require('actions-on-google');
 
 const app = dialogflow();
@@ -30,7 +31,6 @@ function loadModel(){
 
 const recetas = loadModel();
 
-
 //Función que devuelve un BrowseCarousel de recetas en función del filtro
 function filterBrowseCarousel(filter){
     let items = [];
@@ -40,16 +40,44 @@ function filterBrowseCarousel(filter){
                 new BrowseCarouselItem({
                     title: recetas[i].name,
                     url: recetas[i].url,
-                    description: recetas[i].description,
                     image: new Image({
                         url: recetas[i].img,
-                        alt: recetas[i].name
+                        alt: recetas[i].name,
+
                     }),
                 }),
             )
         }
     }
     return new BrowseCarousel({
+        title: filter,
+        items: items
+    });
+};
+
+//Función que devuelve un Carousel de recetas en función del filtro
+function filterCarousel(filter){
+    let items = {};
+    let index=0;
+    for(let i=0; i<recetas.length; i++){
+        if(recetas[i].meal_type==filter){
+            items[index] = {
+                optionInfo: {
+                    key: recetas.name,
+                    synonyms: recetas[i].name,
+                  },
+                title: recetas[i].name,
+                url: recetas[i].url,
+                image: new Image({
+                    url: recetas[i].img,
+                    alt: recetas[i].name,
+
+                }),
+            }
+            index++;
+        }
+    }
+    return new Carousel({
         title: filter,
         items: items
     });
@@ -66,6 +94,17 @@ function filterSuggestions(filter){
     console.log(items);
     return items;
 }
+
+//Función que devuelve una receta en función del nombre de la receta
+function getReceta(receta){
+    for(let i=0; i<recetas.length; i++){
+        if(recetas[i].name==receta){
+            return recetas[i];
+        }
+    }
+    return "";
+}
+
 /*app.intent('Default Welcome Intent', (conv, params) => {
     if (params.nombre){
         conv.ask(`Hola, ${params.nombre}`);
@@ -78,7 +117,12 @@ function filterSuggestions(filter){
     }
 });*/
 //Intent de incio muestra una BasicCard con información de la aplicación
-app.intent('Default Welcome Intent', (conv, params) => {
+app.intent('Default Fallback Intent', (conv) => {
+    conv.ask(`Losiento, no le he entendido. Qué receta desea buscar?`);
+    conv.ask(new Suggestions('Volver'));
+});
+//Intent de incio muestra una BasicCard con información de la aplicación
+app.intent('Default Welcome Intent', (conv) => {
     conv.ask(`Bienvenido. Que recetas quieres buscar?.`);
     conv.ask(new BasicCard({
         text: `En esta aplicación econtratas recetas de todo tipo y para todas las personas. 
@@ -89,34 +133,81 @@ app.intent('Default Welcome Intent', (conv, params) => {
         url: HOSTING + 'recetasLogo.png',
         alt: 'Recetas de cocina logo',
         }),
-        display: 'CROPPED',
+        display: 'DEFAULT',
         }));
     conv.ask(new Suggestions(['Entrante','Plato principal','Segundo palto']));
 });
 
 //Mostrar las recetas clasificadas como entrantes
-app.intent('Entrante', (conv, params) => {
-    //let recipeSuggestions = filterSuggestions('Entrante');
+app.intent('Entrante', (conv) => {
     conv.ask(`A quí podras encontrar diferectes platos para entrante.`);
-    conv.ask(filterBrowseCarousel('Entrante'));
+    conv.ask(filterCarousel('Entrante'));
     conv.ask(new Suggestions('Volver'));
-    //conv.ask(new Suggestions(recipeSuggestions));
 });
 
 //Mostrar las recetas clasificadas como plato principal
-app.intent('Plato principal', (conv, params) => {
+app.intent('PlatoPrincipal', (conv) => {
     conv.ask(`A quí podras encontrar diferectes platos para plato principal.`);
-    conv.ask(filterBrowseCarousel('Plato principal'));
+    conv.ask(filterCarousel('Plato principal'));
     conv.ask(new Suggestions('Volver'));
-    //conv.ask(new Suggestions(filterSuggestions('Plato principal')))
 });
 
 //Mostrar las recetas clasificadas como segundo plato
-app.intent('Segundo palto', (conv, params) => {
+app.intent('SegundoPalto', (conv) => {
     conv.ask(`A quí podras encontrar diferectes platos para segundo plato.`);
-    conv.ask(filterBrowseCarousel('Segundo palto'));
+    conv.ask(filterCarousel('Segundo palto'));
     conv.ask(new Suggestions('Volver'));
-    //conv.ask(new Suggestions(filterSuggestions('Segundo palto')));
+});
+
+app.intent('Receta', (conv, params, option) => {
+    if(params.receta){
+        let receta = getReceta(params.receta);
+        conv.ask(`La receta que ha buscado es: ` + params.receta );
+        if(receta==""){
+            conv.ask(`Lo sentimos, no encontramos la receta que desea buscar`);
+        }else{
+            conv.ask(new BasicCard({
+                text: `En esta aplicación econtratas recetas de todo tipo y para todas las personas. 
+                Busca la receta que te interesa y intenta cocinarla.  \n`,
+                subtitle: receta.description,
+                title: receta.name,
+                image: new Image({
+                url: receta.img,
+                alt: receta.name,
+                ratio: "square"
+                }),
+                display: 'DEFAULT',
+            }));
+            conv.ask(new LinkOutSuggestion({name: 'Receta url', url: receta.url}))
+        }
+    }else{
+        conv.ask(`Lo sentimos, no encontramos la receta que desea buscar`);
+    }
+    if(option){
+        let receta = getReceta(option);
+        conv.ask(`La receta que ha buscado es: ` + option);
+        console.log(option);
+        if(receta==""){
+            conv.ask(`Lo sentimos, no encontramos la receta que desea buscar`);
+        }else{
+            conv.ask(new BasicCard({
+                text: `En esta aplicación econtratas recetas de todo tipo y para todas las personas. 
+                Busca la receta que te interesa y intenta cocinarla.  \n`,
+                subtitle: receta.description,
+                title: receta.name,
+                image: new Image({
+                url: receta.img,
+                alt: receta.name,
+                ratio: "square"
+                }),
+                display: 'DEFAULT',
+            }));
+            conv.ask(new LinkOutSuggestion({name: 'Receta url', url: receta.url}))
+        }
+    }else{
+        conv.ask(`Lo sentimos, no encontramos la receta que desea buscar`);
+    }
+    conv.ask(new Suggestions('Volver'));
 });
 
 
